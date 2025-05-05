abf2040056b815b9bcf5411587d62950fc6f4b20
Dziyana Yakubuk <dsjakubuk@gmail.com>
2025-05-04 21:20:33 -0400

    Set up a project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import styled from 'styled-components';
+
+const Div = styled.div`
+ text-align: center;
+`;
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div >
+      <i className="fa fa-camera-retro" aria-hidden='true'></i>"
+      <div> 123 </div>
+    </Div> 
   );
-}
-
-export default App;
+};

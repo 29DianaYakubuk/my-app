32e1463420e5537b5a90497da53d0675c8c7ebc4
Dziyana Yakubuk <dsjakubuk@gmail.com>
2025-07-19 16:43:35 -0400

    pagination

--- src/bff/operations/fetch-posts.js
+++ src/bff/operations/fetch-posts.js
@@ -2,18 +2,21 @@
 import { getComments, getPosts } from '../api';
 import { getCommentsCount } from '../utils/get-comments-count';
 
-export const fetchPosts = async () => {
+export const fetchPosts = async (page, limit) => {
     
-    const [posts, comments] = await Promise.all([
-        getPosts(),
+    const [{posts, links}, comments] = await Promise.all([
+        getPosts(page, limit),
         getComments(),
     ]);
 
     return {
         error: null,
-        res: posts.map((post) => ({
+        res: {
+            posts: posts.map((post) => ({
             ...post,
             commentsCount: getCommentsCount(comments, post.id),
         })),
+        links,
+    },
     };
 };
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,34 +1,67 @@
 import { useEffect, useState } from "react";
-import { PostCard } from "./components/post-card/post-card";
+import { Pagination, PostCard } from "./components";
 import { useServerRequest } from "../../hooks/use-server-request";
+import { PAGINATION_LIMIT} from "../../bff/constants/pagination-limit";
+import { getLastPageFromLinks } from "./utils/get-last-page-from-links";
 import styled from "styled-components"; 
 
 const MainContainer = ({className}) => {
     const [posts, setPosts] = useState([]);
+    const [page, setPage] = useState(1);
+    const [lastPage, setLastPage] = useState(1);
     const requestServer = useServerRequest();
 
-    useEffect(() => {
-        requestServer('fetchPosts').then((posts) => {
-            setPosts(posts.res);
-        });
-    }, [requestServer]);
+   useEffect(() => {
+       const fetchData = async () => {
+           try {
+               const response = await requestServer(
+                   'fetchPosts',
+                   page,
+                   PAGINATION_LIMIT,
+               );
+
+               const posts = response?.res?.posts || [];
+               const links = response?.res?.links || '';
+
+               console.log('Загруженные посты:', posts);
+               console.log('Сырые links:', links);
+
+               setPosts(posts);
+               setLastPage(getLastPageFromLinks(links));
+           } catch (error) {
+               console.error('Ошибка при загрузке постов:', error);
+               setPosts([]);
+               setLastPage(1);
+           }
+       };
+
+       fetchData();
+   }, [requestServer, page]);
 
     return (
         <div className={className}>
             <div className="post-list">
-                {posts.map(
-                    ({ id, title, imageUrl, publishedAt, commentsCount }) => (
-                        <PostCard
-                            key={id}
-                            id={id}
-                            title={title}
-                            imageUrl={imageUrl}
-                            publishedAt={publishedAt}
-                            commentsCount={commentsCount}
-                        />
-                    ),
-                )}
+                {Array.isArray(posts) &&
+                    posts.map(
+                        ({
+                            id,
+                            title,
+                            imageUrl,
+                            publishedAt,
+                            commentsCount,
+                        }) => (
+                            <PostCard
+                                key={id}
+                                id={id}
+                                title={title}
+                                imageUrl={imageUrl}
+                                publishedAt={publishedAt}
+                                commentsCount={commentsCount}
+                            />
+                        ),
+                    )}
             </div>
+            <Pagination page={page} lastPage={lastPage} setPage={setPage} />
         </div>
     );
 };
